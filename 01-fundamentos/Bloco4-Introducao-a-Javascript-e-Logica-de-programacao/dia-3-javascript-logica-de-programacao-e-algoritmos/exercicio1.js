309c0ab33371708ad49317a6823e30923e59f8d8
Marcos Danilo <marcosdanilo94@gmail.com>
2021-11-30 15:59:09 -0300

    Cria um quadrado com asteriscos de lado de tamanho n

---
+++ 01-fundamentos/Bloco4-Introducao-a-Javascript-e-Logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,13 @@
+ let n = 50;
+ let aux = '';
+
+ for (i = 1; i <= n; i += 1) {
+     for (j = 1; j <=n; j += 1) {
+         aux += '*';
+     }
+     console.log(aux);
+     aux = '';
+    
+ }
+
+

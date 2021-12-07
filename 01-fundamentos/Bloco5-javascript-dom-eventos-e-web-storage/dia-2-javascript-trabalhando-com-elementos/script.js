52293cd1a6b9b18e238c5951e69a39ea199080ab
Marcos Danilo <marcosdanilo94@gmail.com>
2021-12-07 10:00:26 -0300

    Exercicio 2 ok

--- 01-fundamentos/Bloco5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ 01-fundamentos/Bloco5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,7 +1,14 @@
 // 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 let body = document.body
-
 let h1 = document.createElement('h1');
 
-body.appendChild(h1)
-h1.innerText = 'Exercício 5.2 - JavaScript DOM'
+body.appendChild(h1);
+h1.innerText = 'Exercício 5.2 - JavaScript DOM';
+
+// 2. Adicione a tag main com a classe main-content como filho da tag body ;
+let body2 = document.body;
+console.log(body2);
+let main = document.createElement('main');
+
+body2.appendChild(main)
+main.className = 'main-content'

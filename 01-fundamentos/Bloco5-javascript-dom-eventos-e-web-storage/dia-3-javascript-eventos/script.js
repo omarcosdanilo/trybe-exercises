b58c34c40bc9e1fe9f2089a80f9b7a360a11f162
Marcos Danilo <marcosdanilo94@gmail.com>
2021-12-08 00:26:05 -0300

    implementa um evento de click ao botao mudando a cor dos feriados e retira o evento ao clicar tambem

--- 01-fundamentos/Bloco5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ 01-fundamentos/Bloco5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,15 +1,23 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
-    const dayListItem = document.createElement('li');
+    const dayListItem = document.createElement("li");
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
+}
 
 createDaysOfTheWeek();
 
@@ -20,26 +28,33 @@ createDaysOfTheWeek();
 // ****Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
 // ****Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
 // ****Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
 
-function month (array) {
-  
-  let listDays = document.querySelector('#days');
-  
+function month(array) {
+  let listDays = document.querySelector("#days");
+
   for (let i = 0; i < array.length; i += 1) {
-    let listItem = document.createElement('li');
-  
+    let listItem = document.createElement("li");
+
     listDays.appendChild(listItem);
-  
+
     listItem.innerText = array[i];
-    listItem.classList.add('day')
+    listItem.classList.add("day");
 
     if (array[i] === 24 || array[i] === 25 || array[i] === 31) {
-      listItem.classList.add('holiday')
+      listItem.classList.add("holiday");
     }
-    
-    if (array[i] === 4 || array[i] === 11 || array[i] === 18 || array[i] === 25) {
-      listItem.classList.add('friday')
+
+    if (
+      array[i] === 4 ||
+      array[i] === 11 ||
+      array[i] === 18 ||
+      array[i] === 25
+    ) {
+      listItem.classList.add("friday");
     }
   }
 }
@@ -50,13 +65,36 @@ month(dezDaysList);
 // ****Adicione a este botão a ID "btn-holiday" .
 // ****Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
 
-function hollidays (string) {
-  let btnContainer = document.querySelector('.buttons-container');
-  let myBtn = document.createElement('button');
-  
+function holidays(string) {
+  let btnContainer = document.querySelector(".buttons-container");
+  let myBtn = document.createElement("button");
+
   btnContainer.appendChild(myBtn);
-  
+
   myBtn.innerText = string;
-  myBtn.id = 'btn-holliday'
+  myBtn.id = "btn-holliday";
 }
-hollidays('Feriados');
+holidays("Feriados");
+
+// Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// ***É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+
+function changeDayColor() {
+  let myBtn = document.querySelector("#btn-holliday");
+  myBtn.addEventListener("click", color);
+  
+  function color() {
+    let holidays = document.querySelectorAll(".holiday");
+
+    for (let i = 0; i < holidays.length; i += 1) {
+      if (holidays[i].style.background !== 'red'){
+        holidays[i].style.background = 'red';
+      }
+      else if (holidays[i].style.background === 'red') {
+        holidays[i].style.background = 'rgb(238,238,238)';
+      }
+    }
+  }
+}
+changeDayColor();

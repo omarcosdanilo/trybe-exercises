0fa3da64b9738916c1a9dd4e180aa73dcc51dd95
Marcos Danilo <marcosdanilo94@gmail.com>
2021-12-07 23:08:24 -0300

    Exercicio 1 ok - Cria os dias do calendario dinamicamente e atribui as devidas classes

--- 01-fundamentos/Bloco5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ 01-fundamentos/Bloco5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,32 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+//  1. O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+// ****Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+// ****Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+// ****Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function month (array) {
+  
+  let listDays = document.querySelector('#days');
+  
+  for (let i = 0; i < array.length; i += 1) {
+    let listItem = document.createElement('li');
+  
+    listDays.appendChild(listItem);
+  
+    listItem.innerText = array[i];
+    listItem.classList.add('day')
+
+    if (array[i] === 24 || array[i] === 25 || array[i] === 31) {
+      listItem.classList.add('holiday')
+    }
+    
+    if (array[i] === 4 || array[i] === 11 || array[i] === 18 || array[i] === 25) {
+      listItem.classList.add('friday')
+    }
+  }
+}
+month(dezDaysList);

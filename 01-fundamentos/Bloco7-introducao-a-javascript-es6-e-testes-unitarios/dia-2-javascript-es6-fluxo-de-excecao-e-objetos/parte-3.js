b78a99b37ac0f138064c50b39596dd435e783741
Marcos Danilo <marcosdanilo94@gmail.com>
2022-01-15 15:19:55 -0300

    Parte 3 exercicio 7 ok - cria uma funcao que obtem o valor da chave de acordo com sua posicao no objeto

--- 01-fundamentos/Bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3.js
+++ 01-fundamentos/Bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3.js
@@ -18,29 +18,29 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// 1 - Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
 const addTurno = (obj, chave, valor) => obj[chave] = valor;
 addTurno(lesson2, 'turno', 'noite');
 
 
-// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 const listKeys = (obj) => Object.keys(obj);
 listKeys(lesson2);
 
-// Crie uma função para mostrar o tamanho de um objeto.
+// 3 - Crie uma função para mostrar o tamanho de um objeto.
 const sizeObj = (obj) => Object.keys(obj).length;
 sizeObj(lesson2);
 
-// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const listValues = (obj) => Object.values(obj);
 listValues(lesson2);
 
-// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3.
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3.
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 
-// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 const totalEstudantes = (obj) => {
   let total = 0;
@@ -50,3 +50,7 @@ const totalEstudantes = (obj) => {
   return total;
 }
 totalEstudantes(allLessons);
+
+// 7 - Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+const keyValue = (obj, position) => Object.values(obj)[position];
+keyValue(lesson1, 0);

1245645ea071cfb7edc848805e6187ad3d5a632e
Marcos Danilo <marcosdanilo94@gmail.com>
2022-01-15 14:50:01 -0300

    Parte 3 exercicio 3 ok - cria uma funcao que recebe um objeto como parametro e retorna seu tamanho

--- 01-fundamentos/Bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3.js
+++ 01-fundamentos/Bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3.js
@@ -18,12 +18,16 @@ const lesson3 = {
   turno: 'noite',
 };
 
-//Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
 const addTurno = (obj, chave, valor) => obj[chave] = valor;
 addTurno(lesson2, 'turno', 'noite');
 
 
-//Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 const listKeys = (obj) => Object.keys(obj);
 listKeys(lesson2);
+
+// Crie uma função para mostrar o tamanho de um objeto.
+const sizeObj = (obj) => Object.keys(obj).length;
+sizeObj(lesson2);

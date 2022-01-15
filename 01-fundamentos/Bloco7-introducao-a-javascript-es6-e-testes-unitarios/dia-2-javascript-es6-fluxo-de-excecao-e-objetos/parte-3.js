9b6c31254f29ad6fbf1e56ffae6be1197711b8e7
Marcos Danilo <marcosdanilo94@gmail.com>
2022-01-15 14:47:08 -0300

    Parte 3 exercicio 2 ok - cria uma funçao que recebe um objeto como parametro e lista suas chaves

--- 01-fundamentos/Bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3.js
+++ 01-fundamentos/Bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3.js
@@ -20,8 +20,10 @@ const lesson3 = {
 
 //Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 
-const addTurno = (obj, chave, valor) => {
-  obj[chave] = valor
-}
-addTurno(lesson2, 'turno', 'noite')
-console.table(lesson2)
+const addTurno = (obj, chave, valor) => obj[chave] = valor;
+addTurno(lesson2, 'turno', 'noite');
+
+
+//Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const listKeys = (obj) => Object.keys(obj);
+listKeys(lesson2);

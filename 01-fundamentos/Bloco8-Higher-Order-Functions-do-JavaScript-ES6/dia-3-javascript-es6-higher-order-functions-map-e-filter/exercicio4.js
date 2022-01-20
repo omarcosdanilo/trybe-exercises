190db1a10bb45324ea93b054a2150057cc39f9e9
Marcos Danilo <marcosdanilo94@gmail.com>
2022-01-20 18:49:27 -0300

    Exercicio 4 ok

---
+++ 01-fundamentos/Bloco8-Higher-Order-Functions-do-JavaScript-ES6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio4.js
@@ -0,0 +1,12 @@
+const books = require("./objeto");
+
+const published60Years = (array) => {
+  return array.filter((book) => {
+    return (2022 - book.releaseYear) > 60;
+  }).sort((a, b) => a.releaseYear - b.releaseYear );
+}
+  
+
+
+console.log(published60Years(books))
+

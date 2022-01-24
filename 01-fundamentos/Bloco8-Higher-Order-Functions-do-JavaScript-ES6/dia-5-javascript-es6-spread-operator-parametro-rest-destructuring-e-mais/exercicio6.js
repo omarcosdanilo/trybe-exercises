2161d11df779ba6fff07429d95a18e03956b424f
Marcos Danilo <marcosdanilo94@gmail.com>
2022-01-24 20:24:07 -0300

    dada uma lista de carro, retora um objeto representando o carro - array destructuring e object property shorthand

---
+++ 01-fundamentos/Bloco8-Higher-Order-Functions-do-JavaScript-ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio6.js
@@ -0,0 +1,11 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+
+const toObject = ([carro, marca, ano]) => ({carro, marca, ano});
+
+console.log(toObject(palio));
+console.log(toObject(shelbyCobra));
+console.log(toObject(chiron));

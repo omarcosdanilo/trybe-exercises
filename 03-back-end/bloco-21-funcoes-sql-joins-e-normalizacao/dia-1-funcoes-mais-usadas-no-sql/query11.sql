7dcea4f0937024775ffb39e2186728bf06861aa7
Marcos Danilo <marcosdanilo94@gmail.com>
2022-05-31 12:00:17 -0300

    query 11 ok

---
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/query11.sql
@@ -0,0 +1,4 @@
+-- 11. 🚀 Escreva uma query que atualize a coluna PHONE_NUMBER, de modo que todos os telefones iniciados por 515 agora devem iniciar com 777.
+UPDATE hr.employees
+SET phone_number = REPLACE(phone_number, 515, 777)
+WHERE phone_number like '515%';

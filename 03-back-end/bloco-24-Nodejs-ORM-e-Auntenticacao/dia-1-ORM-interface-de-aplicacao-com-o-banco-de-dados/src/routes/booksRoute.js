83851438ab7a821afcaed8eb6b19c14d58f47f35
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-08 11:41:16 -0300

    feat: da acesso a rota GET /books/:id ao controller getById

--- 03-back-end/bloco-24-Nodejs-ORM-e-Auntenticacao/dia-1-ORM-interface-de-aplicacao-com-o-banco-de-dados/src/routes/booksRoute.js
+++ 03-back-end/bloco-24-Nodejs-ORM-e-Auntenticacao/dia-1-ORM-interface-de-aplicacao-com-o-banco-de-dados/src/routes/booksRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const BooksController = require('../controllers/BooksController');
 
 router.get('/', BooksController.getAll);
+router.get('/:id', BooksController.getById);
 
 module.exports = router;

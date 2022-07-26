e4809f7c57a8a033fb9a31c881e765c2feac0ea3
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-26 18:54:33 -0300

    exercicio 3 ok

--- 03-back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercicios/index.ts
+++ 03-back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercicios/index.ts
@@ -1,5 +1,7 @@
 import { characters, printCharacter } from './exercicio1';
 import YearMoths from './exercicio2';
+import YearSeasons from './exercicio3';
 
 characters.forEach(printCharacter);
 console.log(YearMoths.Dezembro);
+console.log(YearSeasons.VERAO);

6e294d418d6899d6761590b6189eddb44f6c3b2a
Marcos Danilo <marcosdanilo94@gmail.com>
2022-07-26 18:48:31 -0300

    Exercicios 1 e 2 ok

---
+++ 03-back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercicios/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const exercicio1_1 = require("./exercicio1");
+const exercicio2_1 = __importDefault(require("./exercicio2"));
+exercicio1_1.characters.forEach(exercicio1_1.printCharacter);
+console.log(exercicio2_1.default.Dezembro);
---
+++ 03-back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercicios/index.ts
@@ -0,0 +1,5 @@
+import { characters, printCharacter } from './exercicio1';
+import YearMoths from './exercicio2';
+
+characters.forEach(printCharacter);
+console.log(YearMoths.Dezembro);

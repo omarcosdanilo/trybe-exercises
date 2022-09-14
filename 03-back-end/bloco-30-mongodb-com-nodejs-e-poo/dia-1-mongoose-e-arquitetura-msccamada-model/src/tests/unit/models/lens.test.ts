8ec0180af983d4db448fa30af239c9b87211300d
Marcos Danilo <marcosdanilo94@gmail.com>
2022-09-13 22:18:13 -0300

    feat: :sparkles: Exercicio 2 - Faça o teste de unidade do método readOne referente classe lens.

--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/src/tests/unit/models/lens.test.ts
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/src/tests/unit/models/lens.test.ts
@@ -1,26 +1,59 @@
 // Exercício 1: 🚀 Faça o teste de unidade do método create referente a classe lens.
 
 import { expect } from "chai";
-import { Model } from "mongoose"
+import { Model, mongo } from "mongoose"
+import mongoose from 'mongoose';
 import Sinon from "sinon"
 import Lens from "../../../models/Lens";
 import { lenMock, lenMockWithId } from "../../mocks/lensMock";
+import Errors from "../../../errors";
 
 
-describe.only('Create lens', () => {
+describe('Lens model', () => {
   const lens = new Lens();
 
-  beforeEach(() => {
-    Sinon.stub(Model, 'create').resolves(lenMockWithId)
+  describe('Create a len' , () => {
+    beforeEach(() => {
+      Sinon.stub(Model, 'create').resolves(lenMockWithId)
+    });
+  
+    afterEach(() => {
+      Sinon.restore();
+    });
+
+    it('successfully created', async () => {
+      const createdLen = await lens.create(lenMock);
+  
+      expect(createdLen).to.be.deep.equal(lenMockWithId)
+    });
   });
 
-  afterEach(() => {
-    Sinon.restore();
-  })
+  describe.only('Searching a len', () => {
+    afterEach(() => {
+      Sinon.restore();
+    });
 
-  it('successfully created', async () => {
-    const createdLen = await lens.create(lenMock);
-    
-    expect(createdLen).to.be.deep.equal(lenMockWithId)
-  })
+    it('Should throw an InvalidMongoId error if an invalid id is passed', async () => {
+      let err: any;
+
+      Sinon.stub(mongoose, 'isValidObjectId').returns(false);
+
+      try {
+        await lens.readOne('id inválido');
+      } catch (catchedError) {
+        err = catchedError;
+      };
+
+      expect(err.message).to.be.equal(Errors.InvalidMongoId)
+    });
+
+    it('Should return a len if a valid id is passed', async () => {
+      Sinon.stub(mongoose, 'isValidObjectId').returns(true);
+      Sinon.stub(Model, 'findOne').resolves(lenMockWithId);
+
+      const foundedLen = await lens.readOne(lenMockWithId._id);
+
+      expect(foundedLen).to.be.deep.equal(lenMockWithId);
+    });
+  });
 })

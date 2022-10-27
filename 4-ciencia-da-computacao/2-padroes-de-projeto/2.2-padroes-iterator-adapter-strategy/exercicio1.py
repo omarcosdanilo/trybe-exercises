68c45e9d8dc8d10093d0e0e33f2e5853d93e052e
Marcos Danilo <marcosdanilo94@gmail.com>
2022-10-27 14:44:33 -0300

    feat: :sparkles: Exercicio 1 - Cria um adapter para executar o método attackWithSaber do Jedi
    Exercício 1: Abaixo temos parte da implementação de um jogo do mundo de Star Wars. Porém, esse código está com um erro. Encontre-o e corrija-o sem alterar o código das classes de personagens (Soldier e Jedi).

---
+++ 4-ciencia-da-computacao/2-padroes-de-projeto/2.2-padroes-iterator-adapter-strategy/exercicio1.py
@@ -0,0 +1,35 @@
+class Soldier:
+    def __init__(self, level):
+        self.level = level
+
+    def attack(self):
+        return self.level * 1
+
+
+class Jedi:
+    def __init__(self, level):
+        self.level = level
+
+    def attackWithSaber(self):
+        return self.level * 100
+
+
+class JediAdapter:
+    def __init__(self, character: Jedi):
+        self.character = character
+
+    def attack(self):
+        return self.character.attackWithSaber()
+
+
+class StarWarsGame:
+    def __init__(self, character):
+        self.character = character
+
+    def fight_enemy(self):
+        print(f"You caused {self.character.attack()} of damage to the enemy")
+
+
+StarWarsGame(Soldier(5)).fight_enemy()
+# StarWarsGame(Jedi(20)).fight_enemy()
+StarWarsGame(JediAdapter(Jedi(20))).fight_enemy()
